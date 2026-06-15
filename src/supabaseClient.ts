d59ef1c2c066660cff7a8555ef77c489500c9760
albash-akhtar <albash@joinfleek.com>
2026-06-15 15:43:54 +0500

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = 'https://qefkpawkljalbevkyxtn.supabase.co';
-// Aapki asli default publishable key
-const key = 'sb_publishable_DAK1eoT1c-3w_T0-NG3bnw_6rPXTGY8'; 
+// Asli correct URL (Typo fix ho gaya)
+const url = 'https://gefkpawkljalbevkxytn.supabase.co';
+
+// Aapki asli 'anon' JWT API key (4th wali)
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZmtwYXdrbGphbGJldmt4eXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjQ5ODgsImV4cCI6MjA5Njk0MDk4OH0.2MC8c4HpKYbBfO_0FCE53_nnwkN7nhqjYIAbvKGHSZE';
 
 export const supabase = createClient(url, key);
