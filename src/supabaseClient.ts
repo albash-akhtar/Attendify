156297ab3c36091f5fe79ea9c08600a2080d8311
albash-akhtar <albash@joinfleek.com>
2026-06-15 15:43:37 +0500

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Aapke project ki direct configurations
 const url = 'https://qefkpawkljalbevkyxtn.supabase.co';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZmtwYXdrbGphbGJldmt5eHRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzMTU4MTksImV4cCI6MjAzMTg5MTgxOX0.X2E5cl9U_WcZ5W8B-9Lp1V7hC6k2m5B3N_W8X9Z9b88'; 
+// Aapki asli default publishable key
+const key = 'sb_publishable_DAK1eoT1c-3w_T0-NG3bnw_6rPXTGY8'; 
 
 export const supabase = createClient(url, key);
